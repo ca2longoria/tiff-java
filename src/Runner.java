b897d079b4ce4a999cd5b72c29be93896508e46c
ca2longoria <ca2longoria@gmail.com>
2013-12-04 12:47:09 -0600

    fixes #2 Added options handling
    Option handling added.  Also, options src and dst have been created.

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,8 @@
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import net.synapsehaven.tiff.*;
+import net.synapsehaven.tiff.Tiff;
 
 public class Runner
 {
@@ -10,9 +11,49 @@ public class Runner
 		if (args.length <= 0)
 			System.exit(1);
 		
-		System.out.println(Arrays.toString(args));
+		final String sep = System.getProperty("file.separator");
 		
-		Tiff tiffy = new Tiff(Arrays.copyOfRange(args, 1, args.length));
-		tiffy.save(args[0]);
+		Map<String,String> options = new HashMap<String,String>();
+		final int argsBegin = parseOptions(options,args);
+		
+		for (int i=argsBegin+1; i < args.length; ++i)
+			args[i] =
+				(options.containsKey("src")
+					? options.get("src") + sep : "")
+				+ args[i];
+		
+		Tiff tiffy = new Tiff(Arrays.copyOfRange(args, argsBegin+1, args.length));
+		tiffy.save(
+			(options.containsKey("dst")
+				? options.get("dst") + sep : "")
+			+ args[argsBegin]);
+	}
+	
+	private static int parseOptions(Map<String,String> map, String[] args)
+	{
+		if (map == null)
+			map = new HashMap<String,String>();
+		
+		String s = null;
+		int i = 0;
+		for (; i < args.length; ++i)
+		{
+			s = args[i];
+			
+			if (s.indexOf("-") != 0) break;
+			s = args[i].substring(1);
+			
+			if (s.equals("src"))
+			{
+				// Source directory
+				map.put("src", args[++i]);
+			}
+			else if (s.equals("dst"))
+			{
+				// Destination directory
+				map.put("dst", args[++i]);
+			}
+		}
+		return i;
 	}
 }
